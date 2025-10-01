b5a2602057f98e3020e03541bf8671ce9edfe8fc
oliverpraznovszky <praznovszkyoli@gmail.com>
2025-10-01 04:44:53 +0200

    Sajat frontend tesztelese

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>TradeByte</h1>
+        <p>Ez a kezdőlap</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
